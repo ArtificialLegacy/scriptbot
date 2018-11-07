37662f78ec0c4c8696b364698318da75d10562cd
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-07 18:43:17 -0500

    Update messages.mjs

--- base/index/messages.mjs
+++ base/index/messages.mjs
@@ -1,1 +1,7 @@
+const Messages = {
+  "noPerms": "You don't have permission to use this command.",
+  "coolActive": "Command cooldown is still active!",
+  "coolFin": "Command cooldown is finished!",
+};
 
+export default Messages;
