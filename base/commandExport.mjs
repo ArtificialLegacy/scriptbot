43ef276434aafd47a2a99f27aecf84b1267535cd
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-08 18:01:28 -0500

    started setup

--- base/commandExport.mjs
+++ base/commandExport.mjs
@@ -1,1 +1,17 @@
+// Import commands
 
+import botinfo from '../commands/botinfo.mjs';
+
+
+
+// Compact into object
+
+const commands = {
+  "botinfo": botinfo,
+};
+
+
+
+// Export
+
+export default commands;
