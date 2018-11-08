8a58a820311ee7edc436567fe35dfaff867f0467
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-07 19:11:39 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,21 @@
-const settings = {
+const Discord = require('discord.js');
+const fs = require('fs');
+const client = new Discord.Client();
+const Enmap = require('enmap');
+const EnmapLevel = require('enmap-level');
 
+const data = new Enmap({
+  provider: new EnmapLevel({
+    name: 'data' 
+  });
+});
+
+
+const settings = {
+  "Discord": Discord,
+  "fs": fs,
+  "client": client,
+  "data": data,
 };
 
-export default settings;
+module.exports = settings;
