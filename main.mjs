5f39d32dffc95c8123a614eebeb59452d289b98f
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-08 12:31:07 -0500

    Added command handler

--- main.mjs
+++ main.mjs
@@ -32,8 +32,41 @@ client.on("guildMemberAdd", async () => {
 
 });
 
-client.on("message", async () => {
-
+client.on("message", async (tempMessage) => {
+  
+  // Command handler
+  
+    // Parse command
+  
+  let messageArray = tempMessage.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+  
+    // Get prefix
+  
+  let prefixes = data.get("prefixes");
+  
+  if(!prefixes[tempMessage.guild.id]){
+    prefixes[tempMessage.guild.id] = {
+      "prefix": "!";
+    };
+  }
+  
+    // Check if the command can/should run
+  
+  if(message.author.bot) return;
+  if(message.channel.type === "dm"){
+    return;
+  };
+  if(!message.content.startsWith(prefixes[tempMessage.guild.id][tempMessage.author.id].prefix)) return;
+  
+    // Try to run the command
+  
+  if(commands.cmd){
+    commands.cmd.run(tempMessage, args);
+  } else {
+    return;
+  }
 });
 
 
