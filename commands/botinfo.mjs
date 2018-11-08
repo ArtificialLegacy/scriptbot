f8e7127a593f4b7b3770f5caa5352f16920389f6
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-08 17:25:09 -0500

    Update botinfo.mjs

--- commands/botinfo.mjs
+++ commands/botinfo.mjs
@@ -1,1 +1,25 @@
+// Import
 
+import settings from '../config.js';
+import Command from '../base/command.mjs';
+
+
+
+// Setup
+
+const Discord = settings.Discord;
+const data = settings.data;
+const client = settings.client;
+
+
+
+// Main
+
+const botinfo = new Command()
+  .setName("botinfo")
+  .setCategory("info")
+  .setDescription("Shows information about the bot.")
+  .setCooldown(60);
+  .setAction((tempMessage) => {
+    let embed = new Discord.RichEmbed();
+  });
